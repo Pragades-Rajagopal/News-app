5222039b9a10c3d53001a4d41314764186e8ce94
Pragades-Rajagopal <pragadesgoldsmith@outlook.com>
2024-09-10 23:09:29 +0530

    adds daily news page

--- lib/features/daily_news/data/models/article.dart
+++ lib/features/daily_news/data/models/article.dart
@@ -5,25 +5,25 @@ import 'package:news_app/features/daily_news/domain/entities/article.dart';
 
 class ArticleModel extends ArticleEntity {
   const ArticleModel({
-    int? id,
-    String? author,
-    String? title,
-    String? description,
-    String? url,
-    String? urlToImage,
-    String? publishedAt,
-    String? content,
+    super.id,
+    super.author,
+    super.title,
+    super.description,
+    super.url,
+    super.urlToImage,
+    super.publishedAt,
+    super.content,
   });
 
   factory ArticleModel.fromJson(Map<String, dynamic> map) {
     return ArticleModel(
-      author: map["author"] ?? '',
-      title: map["title"] ?? '',
-      description: map["description"] ?? '',
-      url: map["url"] ?? '',
-      urlToImage: map["urlToImage"] ?? '',
-      publishedAt: map["publishedAt"] ?? '',
-      content: map["content"] ?? '',
+      author: map['author'] ?? '',
+      title: map['title'] ?? '',
+      description: map['description'] ?? '',
+      url: map['url'] ?? '',
+      urlToImage: map['urlToImage'] ?? '',
+      publishedAt: map['publishedAt'] ?? '',
+      content: map['content'] ?? '',
     );
   }
 }
